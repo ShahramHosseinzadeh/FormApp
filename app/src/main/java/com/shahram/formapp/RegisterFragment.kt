d2c93ef945e31c210ca3c67cf96d6779570719e8
Omidreza Bagherian <OmidrezaBaqerian@gmail.com>
2022-08-16 20:55:36 +0430

    Add fragment_register.xml and RegisterFragment.kt in application

---
+++ app/src/main/java/com/shahram/formapp/RegisterFragment.kt
@@ -0,0 +1,11 @@
+package com.shahram.formapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class RegisterFragment:Fragment(R.layout.fragment_register) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
